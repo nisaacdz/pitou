40037f890d0656bdd77ceace5f00e865014316af
nisaacdz <103804474+nisaacdz@users.noreply.github.com>
2023-06-16 18:56:50 +0000

    added declarative macros

--- src/size.rs
+++ src/size.rs
@@ -1,23 +1,64 @@
 pub trait Unit {
     fn from_bytes(bytes: u64) -> Self;
+    fn to_bytes(self) -> u64;
 }
 
-pub struct Bytes(u64);
-pub struct Kilobytes(u64);
-pub struct Megabytes(u64);
-pub struct Gigabytes(u64);
+#[derive(Clone, Copy)]
+pub struct Bytes(f64);
+#[derive(Clone, Copy)]
+pub struct Kilobytes(f64);
+#[derive(Clone, Copy)]
+pub struct Megabytes(f64);
+
+#[derive(Clone, Copy)]
+pub struct Gigabytes(f64);
 
 macro_rules! impl_unit {
     ($type:ty, $val:literal) => {
         impl Unit for $type {
             fn from_bytes(bytes: u64) -> Self {
-                Self(bytes / (1 << $val))
+                let bytes = bytes as f64 / $val;
+                Self(bytes)
+            }
+
+            fn to_bytes(self) -> u64 {
+                (self.0 * $val) as _
             }
         }
     };
 }
 
-impl_unit!(Bytes, 0);
-impl_unit!(Kilobytes, 10);
-impl_unit!(Megabytes, 20);
-impl_unit!(Gigabytes, 30);
+macro_rules! impl_val {
+    ($type:ty) => {
+        impl $type {
+            pub fn val(&self) -> f64 {
+                self.0
+            }
+        }
+    };
+}
+
+macro_rules! impl_dbg {
+    ($type:ty,$dp:literal) => {
+        impl std::fmt::Debug for $type {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(f, "{:.prec$}", self.0, prec = $dp) // Display two decimal places
+            }
+        }
+    };
+}
+
+impl_unit!(Bytes, 1f64);
+impl_unit!(Kilobytes, 1024f64);
+impl_unit!(Megabytes, 1048576f64);
+impl_unit!(Gigabytes, 1073741824f64);
+
+impl_dbg!(Bytes, 0);
+impl_dbg!(Kilobytes, 2);
+impl_dbg!(Megabytes, 2);
+impl_dbg!(Gigabytes, 2);
+
+impl_val!(Bytes);
+impl_val!(Kilobytes);
+impl_val!(Megabytes);
+impl_val!(Gigabytes);
