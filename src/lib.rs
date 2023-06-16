121f0bdc8c1a5b619115a0b47f26276f8111b968
nisaacdz <103804474+nisaacdz@users.noreply.github.com>
2023-06-16 10:32:46 +0000

    added module level doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,8 @@
+//! # pitu
+//! **pitu** is a file system implementation that is independent of the underlining operating system.
+//!
+//! It provides high level features like **Metadata** and **PathBuf**.
+
+
 pub mod fs;
 pub mod size;
