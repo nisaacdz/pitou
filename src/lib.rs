dfb8cf47f9991be6c877b5b992579e903ae4ded8
nisaacdz <103804474+nisaacdz@users.noreply.github.com>
2023-06-19 21:45:33 +0000

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+//! # pitou
+//! **pitou** is a file system implementation that is independent of the underlining operating system.
+//!
+//! It provides high level features like **Metadata** and **PathBuf**.
+
+
+mod fs;
+
+pub use fs::*;
+
+pub mod units;
+
+//#[cfg(feature = "drive")]
+pub mod drive;
